6efbc3289cc18de2e3cb8b4549aec447d40a551f
Sam Perry <samuel.perry89@gmail.com>
2017-06-05 12:55:28 +0100

    Arrnged segmentations into 4*N array

--- src/generateFeatures.py
+++ src/generateFeatures.py
@@ -1,5 +1,8 @@
+from __future__ import division
 import numpy as np
 import pandas
+import pysndfile
+import matplotlib.pyplot as plt
 import pdb
 
 def parse_segmentation_file(segPath):
@@ -19,7 +22,21 @@ def parse_segmentation_file(segPath):
 
 def calculateFeatures(name, audioPath, segPath):
     segmentation = parse_segmentation_file(segPath)
-    pdb.set_trace()
+    # Get audio data from PCG file
+    audioFile = pysndfile.PySndfile(audioPath, 'r')
+    audioData = audioFile.read_frames()
+    segData = segmentation['data']
+
+    # Organise segments into a 4*N array, where each column represents the S1,
+    # Systole, S2 and Diastole segments for N rows of complete heart cycles
+    firstS1 = np.where(segData[:, 1] == 1)[0][0]
+    lastDiastole = np.where(segData[:, 1] == 4)[0][-1]
+    segs = segData[firstS1:lastDiastole+1, 0].reshape(segData[firstS1:lastDiastole+1, 1].size // 4, 4)
+    '''
+    plt.plot(audioData)
+    plt.step(segData[:, 0]*int(segmentation['originalSR']/segmentation['downsampledSR']), segData[:, 1])
+    plt.show()
+    '''
 
 def generateFeatures(dataFilepaths, output_dir):
     '''

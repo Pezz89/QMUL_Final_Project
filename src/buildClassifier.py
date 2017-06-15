ba42915403c8af6f3853bf9b1530f898d02f56f4
Sam Perry <u1265119@unimail.hud.ac.uk>
2017-06-15 14:52:59 +0100

    Implemented grouped k-fold cross-validation

--- src/buildClassifier.py
+++ src/buildClassifier.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GroupKFold
+
 import numpy as np
 import pdb
 import logging
@@ -18,7 +19,25 @@ def buildClassifier(features, classifications):
     logger.info("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
     logger.info("Accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
     '''
+    groups = generateGroups(features)
     # Evaluate model using startified cross-validation
-    scores = cross_val_score(forest, features, classifications, cv=10)
+    scores = cross_val_score(forest, features, classifications, groups, cv=GroupKFold(n_splits=int(np.max(groups)+1)))
     logging.info("Cross-validation scores: {}".format(scores))
     logging.info("Average Cross-validation score: {}".format(np.mean(scores)))
+
+
+'''
+Generate groups for a pandas DataFrame using keys for splitting
+'''
+def generateGroups(frame):
+    groups = np.empty(frame.index.size)
+
+    groupKeys = {}
+    i = 0
+    for ind, key in enumerate(frame.index):
+        groupChar = key[0]
+        if groupChar not in groupKeys:
+            groupKeys[groupChar] = i
+            i += 1
+        groups[ind] = groupKeys[groupChar]
+    return groups

074014c45f33956ecfa52ff173c324ca129ba351
Sam Perry <u1265119@unimail.hud.ac.uk>
2017-08-16 12:40:36 +0100

    Added stacking classifier as primary classifier

--- src/buildClassifier.py
+++ src/buildClassifier.py
@@ -4,10 +4,9 @@ from sklearn.model_selection import LeaveOneGroupOut
 from sklearn.metrics.scorer import make_scorer
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
-
 from sklearn.decomposition import KernelPCA as KPCA, PCA
 
-
+from sklearn.tree import DecisionTreeClassifier
 # k nearest neighbours
 from sklearn.neighbors import KNeighborsClassifier
 # support vector machine classifier
@@ -18,6 +17,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
 from sklearn.pipeline import Pipeline
+
+from mlxtend.classifier import StackingCVClassifier
 
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 import optunity
@@ -43,22 +44,30 @@ random_state = np.random.RandomState(42)
 np.random.seed(42)
 
 def buildModel(
-    algorithm,
     worker_log = logging.getLogger(__name__),
     **kwargs
 ):
 
     # Filter out None arguments
     kwargs = {k:v for k,v in kwargs.iteritems() if v is not None}
-    fr_method = kwargs.pop('feature_reduction')
+    #fr_method = kwargs.pop('feature_reduction')
 
-    model_parameters = {k: v for k, v in kwargs.iteritems() if 'fr_' not in k and k != ''}
+    cl1_parameters = {k: v for k, v in kwargs.iteritems() if 'cl1_' in k}
+    cl2_parameters = {k: v for k, v in kwargs.iteritems() if 'cl2_' in k}
+    cl3_parameters = {k: v for k, v in kwargs.iteritems() if 'cl3_' in k}
 
     interger_params = ['n_neighbors', 'max_features', 'max_depth', 'n_estimators']
 
+    cl1_parameters = {k.replace('cl1_', ''): v.replace('cl1_', '') if isinstance(v, six.string_types) else v for k, v in cl1_parameters.iteritems() if 'cl1_' in k}
+    cl2_parameters = {k.replace('cl2_', ''): v.replace('cl2_', '') if isinstance(v, six.string_types) else v for k, v in cl2_parameters.iteritems() if 'cl2_' in k}
+    cl3_parameters = {k.replace('cl3_', ''): v.replace('cl3_', '') if isinstance(v, six.string_types) else v for k, v in cl3_parameters.iteritems() if 'cl3_' in k}
     # Round any float parameters that should be ints
-    for p in set(interger_params).intersection(model_parameters):
-        model_parameters[p] = int(round(model_parameters[p]))
+    for p in set(interger_params).intersection(cl1_parameters):
+        cl1_parameters[p] = int(round(cl1_parameters[p]))
+    for p in set(interger_params).intersection(cl2_parameters):
+        cl2_parameters[p] = int(round(cl2_parameters[p]))
+    for p in set(interger_params).intersection(cl3_parameters):
+        cl3_parameters[p] = int(round(cl3_parameters[p]))
 
 
 
@@ -71,12 +80,29 @@ def buildModel(
         'adaboost': AdaBoostClassifier
     }
 
-    worker_log.debug("Building model: {}, with parameters:".format(algorithm).ljust(92))
-    worker_log.debug(" ".join(['{0}: {1}'.format(k, v) for k, v in model_parameters.iteritems()]).ljust(92))
-    # Build model
-    model = model_methods[algorithm](**model_parameters)
+    worker_log.debug("Building model: {}, with parameters:".format('Stacking Classifier').ljust(92))
+    worker_log.debug(" ".join(['{0}: {1}'.format(k, v) for k, v in cl1_parameters.iteritems()]).ljust(92))
+    worker_log.debug(" ".join(['{0}: {1}'.format(k, v) for k, v in cl2_parameters.iteritems()]).ljust(92))
+    worker_log.debug(" ".join(['{0}: {1}'.format(k, v) for k, v in cl3_parameters.iteritems()]).ljust(92))
 
-    pipe_components = [("imputer", preprocessing.Imputer()), ("scaler", preprocessing.MinMaxScaler()), ("model", model)]
+    cl1_algorithm = cl1_parameters.pop('algorithm')
+    cl2_algorithm = cl2_parameters.pop('algorithm')
+    cl3_algorithm = cl3_parameters.pop('algorithm')
+
+    if cl1_algorithm == 'adaboost':
+        cl1_parameters['base_estimator'] = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+    if cl2_algorithm == 'adaboost':
+        cl2_parameters['base_estimator'] = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+    if cl3_algorithm == 'adaboost':
+        cl3_parameters['base_estimator'] = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+
+    # Build model
+    clf1 = model_methods[cl1_algorithm](**cl1_parameters)
+    clf2 = model_methods[cl2_algorithm](**cl2_parameters)
+    clf3 = model_methods[cl3_algorithm](**cl3_parameters)
+    lr = LogisticRegression()
+
+    pipe_components = [("imputer", preprocessing.Imputer()), ("scaler", preprocessing.MinMaxScaler()), ("model", StackingCVClassifier(classifiers=[clf1, clf2, clf3], meta_classifier=lr))]
     pipe = Pipeline(pipe_components)
 
     return pipe
@@ -85,7 +111,7 @@ def buildModel(
 def parameterReduction(model, worker_log = logging.getLogger(__name__), **kwargs):
     # Filter out None arguments
     kwargs = {k:v for k,v in kwargs.iteritems() if v is not None}
-    fr_method = kwargs.pop('feature_reduction')
+    #fr_method = kwargs.pop('feature_reduction')
     #fr_method = kwargs.pop('feature_reduction')
     # Strip fr prefix substring used for distinguishing reduction parameters
     # from model parameters in optimization code
@@ -94,13 +120,13 @@ def parameterReduction(model, worker_log = logging.getLogger(__name__), **kwargs
     for p in set(interger_params).intersection(reduction_parameters):
         reduction_parameters[p] = int(round(reduction_parameters[p]))
 
-    reduction_methods = {
-        "PCA": None,
-        "None": None
-    }
+    #reduction_methods = {
+    #    "PCA": None,
+    #    "None": None
+    #}
     pipe_components = [("model", model)]
-    if reduction_methods[fr_method]:
-        pipe_components.insert(0, ("reduction", reduction_methods[fr_method](n_components=40, **reduction_parameters)))
+    #if reduction_methods[fr_method]:
+    #    pipe_components.insert(0, ("reduction", reduction_methods[fr_method](n_components=40, **reduction_parameters)))
     pipe = Pipeline(pipe_components)
     worker_log.debug("Feature reduction method: {}, with parameters:".format(fr_method).ljust(92))
     worker_log.debug(" ".join(['{0}: {1}'.format(k, v) for k, v in reduction_parameters.iteritems()]).ljust(92))
@@ -163,7 +189,7 @@ def scoreOptimizedModel(features, classifications, groups, train_features, test_
     algorithm = latestSolution.pop('algorithm')
     train_features = train_features.ix[:, latestFeatures]
     test_features = test_features.ix[:, latestFeatures]
-    model = buildModel(algorithm, **latestSolution)
+    model = buildModel(**latestSolution)
     model = parameterReduction(model, **latestSolution)
     model.fit(train_features, train_classifications)
     physionetScorer = make_scorer(score)
@@ -224,11 +250,11 @@ def optimizeClassifierModel(features, classifications, groups, optimization_fpat
     #cross_validator = list(GroupKFold(n_splits=3).split(features,classifications,groups))#int(np.max(groups)+1))
     cross_validator = StratifiedKFold(n_splits=10, random_state=42)
 
-    def dummyWrapper(algorithm='naive-bayes', **kwargs):
+    def dummyWrapper(**kwargs):
         '''Dummy function created for debugging optimization quickly'''
         return (0.5, pd.Index(['diaCent', 'diaDur', 'diaFlat']))
 
-    def optimizationWrapper(algorithm='naive-bayes', **kwargs):
+    def optimizationWrapper(**kwargs):
         process_name = multiprocessing.current_process().name
         worker_log = loggerops.create_logger(
             process_name,
@@ -236,7 +262,7 @@ def optimizeClassifierModel(features, classifications, groups, optimization_fpat
             use_stream_handler=False
         )
         worker_log.propagate = False
-        model = buildModel(algorithm, worker_log=worker_log, **kwargs)
+        model = buildModel(worker_log=worker_log, **kwargs)
         scr, featureLabels = modelFeatureSelection(features, classifications, cross_validator, model, worker_log=worker_log, **kwargs)
         return scr, featureLabels
     # TODO: Used for quickly debugging particle swarm optimization, remove for
@@ -246,24 +272,43 @@ def optimizeClassifierModel(features, classifications, groups, optimization_fpat
     # Define search space, providing model names and parameter ranges to search
     # for best solution
     search = {
-        'algorithm': {
-            'SVM': {
-                'kernel': {
-                    'linear': {'C': [0, 2]},
-                    'rbf': {'gamma': [0, 1], 'C': [0, 10]},
-                    'poly': {'degree': [2, 5], 'C': [0, 50], 'coef0': [0, 1]}
+        'cl1_algorithm': {
+            'cl1_SVM': {
+                'cl1_kernel': {
+                    'cl1_linear': {'cl1_C': [0, 2]},
+                    'cl1_rbf': {'cl1_gamma': [0, 1], 'cl1_C': [0, 10]},
+                    'cl1_poly': {'cl1_degree': [2, 5], 'cl1_C': [0, 50], 'cl1_coef0': [0, 1]}
                 }
             },
-            'adaboost': {
-                'n_estimators': [10, 60],
-            },
-            'naive-bayes': None,
+            'cl1_naive-bayes': None,
         },
-        'feature_reduction': {
-            'PCA': None,
-            'None': None
+        'cl2_algorithm': {
+            'cl2_SVM': {
+                'cl2_kernel': {
+                    'cl2_linear': {'cl2_C': [0, 2]},
+                    'cl2_rbf': {'cl2_gamma': [0, 1], 'cl2_C': [0, 10]},
+                    'cl2_poly': {'cl2_degree': [2, 5], 'cl2_C': [0, 50], 'cl2_coef0': [0, 1]}
+                }
+            },
+            'cl2_naive-bayes': None,
+        },
+        'cl3_algorithm': {
+            'cl3_SVM': {
+                'cl3_kernel': {
+                    'cl3_linear': {'cl3_C': [0, 2]},
+                    'cl3_rbf': {'cl3_gamma': [0, 1], 'cl3_C': [0, 10]},
+                    'cl3_poly': {'cl3_degree': [2, 5], 'cl3_C': [0, 50], 'cl3_coef0': [0, 1]}
+                }
+            },
+            'cl3_naive-bayes': None,
         }
     }
+
+    '''
+    'cl1_adaboost': {
+        'cl1_n_estimators': [10, 40],
+    },
+    '''
     '''
             'random-forest': {
                 'n_estimators': [10, 30],
